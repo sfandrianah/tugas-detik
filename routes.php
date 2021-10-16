ea1fcc6be980a65c9f26a260aad4ba784f826988
Syahrial Fandrianah <sfandrianah2@gmail.com>
2021-10-16 23:09:32 +0700

    Revert "no message"
    This reverts commit aea19d01e7e26aac5dfb7d9503e1e3e1416eea44.

--- routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-\app\Util\Routes::get("/payment", [\app\controllers\PaymentController::class, "find"]);
-\app\Util\Routes::post("/payment", [\app\controllers\PaymentController::class, "save"]);
